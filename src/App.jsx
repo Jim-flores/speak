e9dd3c0921cc3fc56035390e7362c5bf4905fd3d
Jim Flores <j1mer0528@gmail.com>
2022-10-07 20:50:52 -0500

    project audio added

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useRef, useState } from 'react'
+import './App.css'
+
+function App() {
+  const localVideoRef = useRef()
+  const getUserMedia = (_audio, _video) => {
+    const constraints = {
+      audio: _audio,
+      video: _video,
+    }
+
+    navigator.mediaDevices.getUserMedia(constraints)
+      .then(stream => {
+        localVideoRef.current.srcObject = stream
+      })
+      .catch(e => {
+        console.log('getUserMedia error...', e);
+      })
+  }
+
+  return (
+    <div className="App">
+      <button onClick={() => getUserMedia(true, false)}>Activar microfono y video</button>
+      {/* <button onClick={() => getUserMedia(false, true)}>´Cancel</button> */}
+      <video ref={localVideoRef} autoPlay></video>
+    </div>
+  )
+}
+
+export default App
